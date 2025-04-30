d356e2a7581fa39aa8c2d8310cec53dfbf85b8ab
Carlos Queiroz <chqueiroz2002@gmail.com>
2025-04-30 07:33:16 -0300

    Limite de livros emprestados

--- src/br/com/bibliotecaDev/modelos/Usuario.java
+++ src/br/com/bibliotecaDev/modelos/Usuario.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Usuario extends Pessoa{
     private String matricula;
     private List<Emprestimo> emprestimosAtivos;
-    private  int limiteEmprestimos = 3;
+    private  int limiteEmprestimos = 5;
 
     public Usuario(String nome, String email, String matricula) {
         super(nome, email);
